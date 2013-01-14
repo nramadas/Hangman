9a990b106235b18b27d45a787f2acee8d3e6fbf8
Kyle + Niranjan <nrbramadas@gmail.com>
2013-01-14 15:28:58 -0800

    Created Player classes, added several methods

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,26 @@
 class Hangman
+
   def initialize
+    @secret_guess = @referee.choose_secret
+    @guesser.secret_length = @secret_guess.length
+  end
+
+  def game_board
+    p ['_ '] * @secret_guess.length
   end
 end
 
 class Player
+  attr_accessor :secret_length, :wrong_letters
+
   def initialize
-    
+    @dictionary = load_dictionary
+    @secret_length = nil
+    @wrong_letters = []
+  end
+
+  def load_dictionary(filename = 'dictionary.txt')
+    File.readlines(filename).map { |word| word.chomp.downcase }
   end
 end
 
@@ -13,10 +28,64 @@ class HumanPlayer < Player
   def initialize
     super
   end
+
+  def guess
+    puts "What letter do you want to guess?"
+    gets.chomp.downcase[0]
+  end
+
+  def check_guess
+  end
+
+  def choose_secret
+    puts "What word do you want the computer to guess?"
+    guess = gets.chomp.downcase
+    unless @dictionary.include?(guess)
+      puts "Not found in dictionary, try again."
+      return choose_secret
+    end
+    guess
+  end
 end
 
 class ComputerPlayer < Player
   def initialize
     super
   end
+
+  def guess
+    # guess the letter with the highest frequency
+    # don't guess letters that have already been guessed
+    # narrow dictionary based on correct guesses
+  end
+
+  def register_guess(guess = {:response => :wrong, :letter => nil, :location => []})
+    if guess[:response] == :wrong
+      prune_dictionary(guess[:letter])
+    else
+      narrow_options(guess[:letter], guess[:location])
+    end
+  end
+
+  def check_guess
+  end
+
+  def choose_secret
+    @dictionary.sample
+  end
+
+  def prune_dictionary(letter)
+    @dictionary.select! { |word| !word.include?(letter) }
+  end
+
+  def narrow_options(letter, indices)
+    indices.each do |index|
+      @dictionary.select! { |word| word[index] == letter }
+    end
+  end
+
+  def exclude_words
+    @dictionary.select! { |word| word.length == @secret_length }
+  end
+
 end
