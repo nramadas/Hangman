21a912003d09074d3ab7d5c7fce055608e538e41
App Academy <app-academy@pissarro.att.net>
2013-01-14 14:57:01 -0800

    First commit

---
+++ hangman.rb
@@ -0,0 +1,22 @@
+class Hangman
+  def initialize
+  end
+end
+
+class Player
+  def initialize
+    
+  end
+end
+
+class HumanPlayer < Player
+  def initialize
+    super
+  end
+end
+
+class ComputerPlayer < Player
+  def initialize
+    super
+  end
+end
