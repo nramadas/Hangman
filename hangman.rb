f41dbe1e84ad3c1fe9d8ed2731faf91b707984c5
Kyle + Niranjan <nrbramadas@gmail.com>
2013-01-14 17:24:01 -0800

    Both modes are fully functional

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,79 @@
 class Hangman
 
+  MAX_TURNS = 10
+
   def initialize
+    case choose_mode
+    when 1
+      @guesser = HumanPlayer.new
+      @referee = ComputerPlayer.new
+    when 2
+      @guesser = ComputerPlayer.new
+      @referee = HumanPlayer.new
+    end
     @secret_guess = @referee.choose_secret
     @guesser.secret_length = @secret_guess.length
+    @wrong_letters = []
   end
 
-  def game_board
-    p ['_ '] * @secret_guess.length
+  def play
+    turns = MAX_TURNS
+    game_board = [nil] * @secret_guess.length
+    until lost?(turns)
+      if won?(game_board)
+        puts "YOU WON!"
+        return
+      end
+      print_board(game_board, turns)
+      guess = @guesser.guess
+      validation = @referee.validate_guess(guess)
+      turns -= 1 if validation[:response]==:wrong
+      @guesser.register_validation(validation)
+      game_board = update_game_board(game_board, validation)
+    end
+    puts "YOU LOST! The word was #{@secret_guess}"
+  end
+
+  private
+
+  def update_game_board(game_board, validation)
+    if validation[:location].empty?
+      @wrong_letters << validation[:letter]
+    else
+      validation[:location].each do |location|
+        game_board[location] = validation[:letter]
+      end
+    end
+    game_board
+  end
+
+  def won?(board)
+    false
+    true if @secret_guess == board.join
+  end
+
+  def lost?(turns)
+    false
+    true if turns == 0
+  end
+
+  def choose_mode
+    puts "Choose (1) if you want to be the guesser."
+    puts "Choose (2) if you want to be the ref."
+    gets.chomp.to_i
+  end
+
+  def print_board(board, turns)
+    board.each do |letter|
+      if letter.nil?
+        print "_ "
+      else
+        print "#{letter} "
+      end
+    end
+    puts "\n#{(0...board.length).to_a.join(' ')}"
+    puts "#{turns} turns left"
+    puts "Wrong letters: #{@wrong_letters}"
   end
 end
 
@@ -55,6 +122,12 @@ class HumanPlayer < Player
     end
     guess
   end
+
+  def register_validation(guess = {:response => :wrong,
+                                  :letter => nil,
+                                  :location => []})
+    # the human should be able to do this without assistance
+  end
 end
 
 class ComputerPlayer < Player
@@ -64,13 +137,14 @@ class ComputerPlayer < Player
   end
 
   def guess
+    prune_dict_by_length(@secret_length)
     highest_frequency = 0
     most_common_letter = ""
     ('a'..'z').each do |letter|
       next if @guessed_letters.include?(letter)
       letter_frequency = @dictionary.join.count(letter)
       if letter_frequency > highest_frequency
-        most_common_letter = letter 
+        most_common_letter = letter
         highest_frequency = letter_frequency
       end
     end
@@ -78,7 +152,9 @@ class ComputerPlayer < Player
     most_common_letter
   end
 
-  def register_guess(guess = {:response => :wrong, :letter => nil, :location => []})
+  def register_validation(guess = {:response => :wrong,
+                                  :letter => nil,
+                                  :location => []})
     if guess[:response] == :wrong
       prune_dict_if_wrong(guess[:letter])
     else
@@ -88,7 +164,7 @@ class ComputerPlayer < Player
 
   def validate_guess(letter)
     locations = []
-    @secret_guess.each_with_index do |l, i|
+    @secret_guess.split('').each_with_index do |l, i|
       locations << i if l==letter
     end
     if locations.empty?
@@ -99,8 +175,10 @@ class ComputerPlayer < Player
   end
 
   def choose_secret
-    @dictionary.sample
+    @secret_guess = @dictionary.sample
   end
+
+  private
 
   def prune_dict_if_wrong(letter)
     @dictionary.select! { |word| !word.include?(letter) }
@@ -113,16 +191,16 @@ class ComputerPlayer < Player
     end
   end
 
-  def prune_dict_by_length
-    @dictionary.select! { |word| word.length == @secret_length }
+  def prune_dict_by_length(word_length)
+    @dictionary.select! { |word| word.length == word_length }
   end
 end
 
 # test script
-c = ComputerPlayer.new
-c.secret_length = 5
-c.prune_dict_by_length
-puts c.guess
-puts c.guess
-puts c.guess
-puts c.guess
+# c = ComputerPlayer.new
+# c.secret_length = 5
+# c.prune_dict_by_length
+# puts c.guess
+# puts c.guess
+# puts c.guess
+# puts c.guess
